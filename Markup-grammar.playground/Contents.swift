ce55716413b3541d05a5361ab933c5ce55f40dd5
sogih <sogih92@gmail.com>
2019-03-22 17:19:58 +0900

    markup

---
+++ Markup-grammar.playground/Contents.swift
@@ -0,0 +1,77 @@
+/*:
+# Heading 1 String
+## Heading 2 String
+### Heading 3 String
+*/
+
+/*:
+ This text is above the horizontal rule.
+ - - -
+ And this is below.
+ */
+
+/*:
+ * Item 1, List A
+ * Item 2, List A
+ * Item 1, List B
+ * Item 1, List C
+ */
+
+/*:
+ 1. Cat
+ 3. Dog
+    1. Golden Retriever
+ 2. Llama
+ */
+
+//: Show Swift elements such as `for` and `let` as monspaced code font
+
+/*:
+ This line has a word with *emphasis*.
+ 
+ This line uses _emphasis for the last six words_.
+ */
+
+/*:
+ A strong * (asterisk) is on this line.
+ 
+ __A strong line__.
+ */
+
+
+/*:
+ - Important:
+ "The beginning is the most important part of the work."
+ \
+ –Plato
+ */
+
+/*:
+ - Example: *A simple `for` loop.*\
+ This example shows a `for` loop that prints the numbers 1 to 5.\
+ \
+ `for index in 1...5 {`\
+ `   print("index = \(index)")`\
+ `}`}
+ */
+
+/*:
+ - Note:
+ "There is nothing either good or bad, but thinking makes it so."
+ \
+ \
+ Hamlet in (*Hamlet, 2.2*) by William Shakespeare
+ */
+
+//: \* This is not a bullet item
+//: * This is a bullet item
+
+/*:
+ These two lines
+ wrap
+ 
+ These two lines\
+ do not
+ */
+
+
